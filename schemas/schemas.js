b411ea92eaf13b6f136929a9aec1379cf317d886
BohdanHromchak <gromchakbogdan@gmail.com>
2023-05-31 21:07:34 +0300

    upd user model, add joi emailSchema

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -33,9 +33,16 @@ const loginSchema = Joi.object({
   password: Joi.string().min(6).required(),
 });
 
+const emailSchema = Joi.object({
+  email: Joi.string()
+    .pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+    .required(),
+});
+
 module.exports = {
   contactsAddSchema,
   updateFavoriteSchema,
   registerSchema,
   loginSchema,
+  emailSchema,
 };
